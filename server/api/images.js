581cb5d6a216dcd08ee819b5076d6d47d5806011
Ajeh Emeke <ajemeke@gmail.com>
2018-07-25 23:20:43 +0100

    Cache bursting

--- server/api/images.js
+++ server/api/images.js
@@ -5,21 +5,21 @@ const ImagesApi = function(client) {
 ImagesApi.prototype.getRecentUploads = function (limit = 12) {
     let url = 'images/recent'
     return this.client.get(url, {
-        query: { limit }
+        query: { limit, t: Date.now() }
     })
 }
 
 ImagesApi.prototype.getUserUploads = function (page = 1, perpage = 12) {
     let url = 'user/images'
     return this.client.get(url, {
-        query: { page, perpage }
+        query: { page, perpage, t: Date.now() }
     })
 }
 
 ImagesApi.prototype.getAllUploads = function (page = 1, perpage = 12, search = null) {
     let url = 'images'
     return this.client.get(url, {
-        query: { page, perpage, search }
+        query: { page, perpage, search, t: Date.now() }
     })
 }
 
