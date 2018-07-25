e975cd8975dc5bc62009f9fdb008d00e8862704b
Ajeh Emeke <ajemeke@gmail.com>
2018-07-25 23:29:58 +0100

    Fix wrong config key

--- server/api/images.js
+++ server/api/images.js
@@ -5,21 +5,21 @@ const ImagesApi = function(client) {
 ImagesApi.prototype.getRecentUploads = function (limit = 12) {
     let url = 'images/recent'
     return this.client.get(url, {
-        query: { limit, t: Date.now() }
+        params: { limit, t: Date.now() }
     })
 }
 
 ImagesApi.prototype.getUserUploads = function (page = 1, perpage = 12) {
     let url = 'user/images'
     return this.client.get(url, {
-        query: { page, perpage, t: Date.now() }
+        params: { page, perpage, t: Date.now() }
     })
 }
 
 ImagesApi.prototype.getAllUploads = function (page = 1, perpage = 12, search = null) {
     let url = 'images'
     return this.client.get(url, {
-        query: { page, perpage, search, t: Date.now() }
+        params: { page, perpage, search, t: Date.now() }
     })
 }
 
